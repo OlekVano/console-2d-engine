d51c6c4f482736d4f903f06c5793f305f56672f4
OlekVano <olekvano@gmail.com>
2022-12-06 22:22:47 +0000

    Added the Entity class

---
+++ entity.py
@@ -0,0 +1,7 @@
+class Entity:
+    def __init__(self, sprite, width, height, pos_x, pos_y):
+        self.sprite = sprite
+        self.width = width
+        self.height = height
+        self.pos_x = pos_x
+        self.pos_y = pos_y
