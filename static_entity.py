ea963145c4a139424bcc04c3e720c05ca88e906f
OlekVano <olekvano@gmail.com>
2022-12-06 22:23:19 +0000

    Refactored the StaticEntity class' code

--- static_entity.py
+++ static_entity.py
@@ -1,13 +1,8 @@
 import utils
 
-class StaticEntity:
-    def __init__(self, sprite, width, height, pos_x, pos_y):
-        self.sprite = sprite
-        self.width = width
-        self.height = height
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-    
+from entity import Entity
+
+class StaticEntity(Entity):
     def from_txt(path, width, height):
         sprite = utils.sprites_from_txt(path, width, height, 1)[0]
         return StaticEntity(sprite, width, height, 0, 0)
