91733a4030d8fb18be9ba9d4952cfb2a33afb016
OlekVano <olekvano@gmail.com>
2022-12-07 00:03:13 +0000

    Changed the Entity and StaticEntity classes, that now use the Animation class added in the previous commit

--- entity.py
+++ entity.py
@@ -1,6 +1,6 @@
 class Entity:
-    def __init__(self, sprite, width, height, pos_x, pos_y):
-        self.sprite = sprite
+    def __init__(self, animation, width, height, pos_x, pos_y):
+        self.animation = animation
         self.width = width
         self.height = height
         self.pos_x = pos_x
--- static_entity.py
+++ static_entity.py
@@ -1,13 +1,16 @@
 import utils
 
 from entity import Entity
+from animation import Animation
 
 class StaticEntity(Entity):
     def from_txt(path, width, height):
         sprite = utils.sprites_from_txt(path, width, height, 1)[0]
-        return StaticEntity(sprite, width, height, 0, 0)
+        animation = Animation(width, height, [sprite], 0)
+        return StaticEntity(animation, width, height, 0, 0)
 
     def draw(self, frame):
         for i in range(self.height):
             for j in range(self.width):
-                frame[self.pos_y + i][self.pos_x + j] = self.sprite[i][j]
+                sprite = self.animation.frame()
+                frame[self.pos_y + i][self.pos_x + j] = sprite[i][j]
