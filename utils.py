b55540f47b343d860c47425e8f84df8e5e776b2b
OlekVano <olekvano@gmail.com>
2022-12-08 23:06:30 +0000

    Added argument validation to the sprites_from_txt function

--- utils.py
+++ utils.py
@@ -1,4 +1,32 @@
+import os
+
 def sprites_from_txt(path, width, height):
+
+    #path validation
+    t = type(path)
+    if t != str:
+        raise TypeError(f'sprites_from_txt: path must be of type \'str\', not \'{t}\'')
+    if len(path) <= 4:
+        raise ValueError('sprites_from_txt: invalid path')
+    if path[-4:] != '.txt':
+        raise ValueError('sprites_from_txt: path must lead to a .txt file')
+    if not os.path.exists(path):
+        raise FileNotFoundError(f'sprites_from_txt: no such file or directory: \'{path}\'')
+
+    #width validation
+    t = type(width)
+    if t != int:
+        raise TypeError(f'sprites_from_txt: width must be of type \'int\', not \'{t}\'')
+    if width <= 0:
+        raise ValueError('sprites_from_txt: width must be a positive number')
+    
+    #height validation
+    t = type(height) 
+    if t != int:
+        raise TypeError(f'sprites_from_txt: height must be of type \'int\', not \'{t}\'')
+    if height <= 0:
+        raise ValueError('sprites_from_txt: height must be a positive number')
+
     with open(path, 'r') as f:
         lines = f.readlines()
         sprites = []
