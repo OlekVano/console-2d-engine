2c23be01c28bd727cf99446d5ea72be18e76f9d1
OlekVano <olekvano@gmail.com>
2022-12-12 19:59:42 +0000

    Added main.py file

---
+++ main.py
@@ -0,0 +1,7 @@
+from animated_entity import AnimatedEntity
+from animation_manager import AnimationManager
+from animation import Animation
+from entity import Entity
+from scene import Scene
+from static_entity import StaticEntity
+from utils import *
