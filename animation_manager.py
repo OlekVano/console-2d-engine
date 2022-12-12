4388225c948d1e2c25e6e1d11ba7bacb87841431
OlekVano <olekvano@gmail.com>
2022-12-12 21:38:07 +0000

    Added optional arguments to the AnimationManager's init function

--- animation_manager.py
+++ animation_manager.py
@@ -1,15 +1,16 @@
 class AnimationManager:
-    def __init__(self):
-        self.animations = {}
-        self.default = None
-        self.current = None
-        self.loop = True
+    def __init__(self, animations={}, default=None):
+        self.animations = animations
+        self.default = default
+        self.current = self.default
+        self.loop = False
 
     def add(self, name, animation):
         self.animations[name] = animation
     
     def set_default(self, name):
         self.default = name
+        self.current = self.default
     
     def play_once(self, name):
         self.current = name
